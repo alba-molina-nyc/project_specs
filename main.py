0cf16be347d2a9b556cfd76cf5f5eaa9f4ca55be
Username <albavmolinanyc@gmail.com>
2022-06-21 11:59:02 -0400

    reads input file with read_excel method

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import pandas as pd 
 
-print(pd.__version__)
+# print(pd.__version__)
+
+pd.read_excel('Ex_input_file_02.04.2021.xlsx', index_col=0, header=0)
+
+print(pd.read_excel('Ex_input_file_02.04.2021.xlsx', index_col=0, header=0))
