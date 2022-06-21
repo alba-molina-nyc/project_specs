48048d330d031dc9f0853bc65f78d76531d6e3c2
Username <albavmolinanyc@gmail.com>
2022-06-21 14:29:02 -0400

    change date format

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pandas as pd 
-from datetime import date, datetime
+from datetime import datetime
+import os
+path = '/Users/albamolina/take1/data'
+# input_file = pd.read_excel('Ex_input_file_02.04.2021.xlsx', index_col=0, header=0)
+# print(input_file)
 
-print(datetime.now())
+# 1. (Python) Extract a time stamp portion from the input file name and display it in MM/DD/YYYY format when
 
-# print(pd.__version__)
+def change_input_filename():
+    input_file_list = []
+    for input_file in os.listdir(path):
+        if input_file.endswith('.xlsx'):
+            input_date = input_file[14:]
+            print(input_date, 'input date')
+            new_date = input_date.strftime("%Y-%m-%d")
+            print(new_date, new_date)
+         
+            filename = input_file[:14]
+            print(filename, 'filename')
+            print(input_file)
+            print(input_file_list)
 
-pd.read_excel('Ex_input_file_02.04.2021.xlsx', index_col=0, header=0)
+change_input_filename()
 
-# print(pd.read_excel('Ex_input_file_02.04.2021.xlsx', index_col=0, header=0))
+print(input_file.columns)
