f111159289351aabc87e69578f0fac4210918bdb
Username <albavmolinanyc@gmail.com>
2022-06-21 16:47:30 -0400

    creates connection usingcreate_engine, pd && creates ex_input db to start loading data

--- importsql.py
+++ importsql.py
@@ -1,28 +1,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
-import psycopg2
-import sys
 
-# Connection parameters
-param_dic = {
-    "host"      : "localhost",
-    "database"  : "vidrio",
-    # "user"      : "vid",
-    "user"      : "albamolina",
-    "password"  : "yourpassword"
-}
+engine = create_engine('postgresql://albamolina@localhost/vidrio')
+print(engine)
+df = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx')
+print(df)
 
-def connect(params_dic):
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params_dic)
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-        sys.exit(1) 
-    print("Connection successful")
-    return conn
+df.to_sql('ex_input', con=engine)
 
-connect(param_dic)
+
