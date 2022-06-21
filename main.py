d20c4b377546a8aeb6122ba974f2614b0748dc32
Username <albavmolinanyc@gmail.com>
2022-06-21 15:30:17 -0400

    add note about edge case my get_date() solution for Anatoliy

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import pandas as pd
 from datetime import datetime, date
 import os
 path = '/Users/albamolina/take1/data'
-# input_file = pd.read_excel('Ex_input_file_02.04.2021.xlsx', index_col=0, header=0)
-# print(input_file)
 
-# 1. (Python) Extract a time stamp portion from the input file name and display it in MM/DD/YYYY format when
-
+"""
+# 1 (Python) Extract a time stamp portion from the input file name and display it in MM/DD/YYYY format when
+"""
 
 def get_date():
     date_form_list = []
     for input_file in os.listdir(path):
-        date_holder = input_file[14:24:1]
-        for i in date_holder:
+        input_time_stamp = input_file[14:24:1]
+        for i in input_time_stamp:
             if i in '0123456789':
                 date_form_list.append(i)
             else:
@@ -21,40 +20,22 @@ def get_date():
                     date_form_list.append(i)
         date_str_form = ''.join(str(i) for i in date_form_list)
         filename = input_file[:14] + date_str_form
-        print(filename, 'skldfjosifj')
-        print(date_str_form, 'str form')
-      
-
-    
-    print(date_form_list, 'dateholder')
-    print(''.join(str(i) for i in date_form_list), 'joinerjoiner')
-    return ''.join(str(i) for i in date_form_list)
+    print(filename)
+    return filename
                 
 get_date()
 
-# def change_filename():
+"""
+--------------------------------------------------------------------------------------------------------------
+NOTE: assumptions I am making in my above get_date():
+1. I am assuming that I will always find the time stamp at the slicing done in input_time_stamp, what if the date appears at the beginning? (The reason I did it that way is because there is a trailing '.' at the end of the filename for the '.xlsx' 
+- so this is an edge case I would further consider if I was not pressed for time 
+i.e. - 
+
+if i in '.' and i.next not in '0123456789' => then i would not append to the list 
+but if i in '.' and i.next in '0123456789' -> then yes append to the list )
+--------------------------------------------------------------------------------------------------------------
+"""
 
 
 
-
-# def change_input_filename():
-#     input_file_list = []
-#     for input_file in os.listdir(path):
-#         if input_file.endswith('.xlsx'):
-#             input_date = slice(14, 24, 1)
-#             new_date = input_file[input_date].strptime(input_date, "%Y-%m-%d %H:%M:%S.%f" )
-#             # new_date = input_date.strftime(input_date, "%Y-%m-%d" )
-#             print(input_file[input_date], 'we are here')
-#             print(new_date)
-#             print(input_date, 'input date')
-#             # new_date = input_date.strftime("%Y-%m-%d")
-#             # print(new_date, new_date)
-         
-#             # filename = input_file[:14]
-#             # print(filename, 'filename')
-#             # print(input_file)
-#             # print(input_file_list)
-
-# change_input_filename()
-
-# print(input_file.columns)
