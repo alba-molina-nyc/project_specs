2bddc8b555c65cd245e459e7f5a0cb6be4106364
Username <albavmolinanyc@gmail.com>
2022-06-21 17:26:25 -0400

    print columns -- good place to pick up tomorrow

--- importsql.py
+++ importsql.py
@@ -8,14 +8,12 @@ engine = create_engine('postgresql://albamolina@localhost/vidrio')
 """2. (Python) Read the ex_input file and save its content to database."""
 
 df_ex_input = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx')
-df_ex_input.to_sql('Ex_input_file_02.04.2021.xlsx', con=engine)
-
-
+# df_ex_input.to_sql('Ex_input_file_02.04.2021.xlsx', con=engine)
 
 """3. (Python) Read the mapping file and save its content to database."""
 
 df_ex_mapping = pd.read_excel('data/Ex_mapping_file.xlsx')
-df_ex_mapping.to_sql('data/Ex_mapping_file.xlsx', con=engine)
+# df_ex_mapping.to_sql('data/Ex_mapping_file.xlsx', con=engine)
 
 
 """NOTE about step 2 and step 3 
@@ -25,3 +23,7 @@ df_ex_mapping.to_sql('data/Ex_mapping_file.xlsx', con=engine)
 3. Furthermore, to build on step 2- I will make it dynamic based on the renaming of the file to fit the format that step 1 asks 
 """
 
+# 4
+
+print(df_ex_input.columns, "=====> df ex col",
+df_ex_mapping.columns,  "=====> df ex mapping col",)
