2b3cd89dc0e42d76ab83eeee5c750a0920348334
Username <albavmolinanyc@gmail.com>
2022-06-21 12:06:41 -0400

    import datetime

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd 
+from datetime import date, datetime
+
+print(datetime.now())
 
 # print(pd.__version__)
 
 pd.read_excel('Ex_input_file_02.04.2021.xlsx', index_col=0, header=0)
 
-print(pd.read_excel('Ex_input_file_02.04.2021.xlsx', index_col=0, header=0))
+# print(pd.read_excel('Ex_input_file_02.04.2021.xlsx', index_col=0, header=0))
