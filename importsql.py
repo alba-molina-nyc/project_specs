44cbc6a17a1697b357d8aa5afcf8411715eb82c9
Username <albavmolinanyc@gmail.com>
2022-06-22 09:53:31 -0400

    get headers from the ex input file

--- importsql.py
+++ importsql.py
@@ -25,5 +25,8 @@ df_ex_mapping = pd.read_excel('data/Ex_mapping_file.xlsx')
 
 # 4
 
-print(df_ex_input.columns, "=====> df ex col",
-df_ex_mapping.columns,  "=====> df ex mapping col",)
+# print(df_ex_input.columns, "=====> df ex col",
+# df_ex_mapping.columns,  "=====> df ex mapping col",)
+
+headers = df_ex_input.columns 
+print(headers)
