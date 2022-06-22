1bdb5153c24645881a40961ea6f3b6a02b18c7d1
Username <albavmolinanyc@gmail.com>
2022-06-22 10:19:27 -0400

    for loops to get particular row

--- importsql.py
+++ importsql.py
@@ -3,25 +3,12 @@ from sqlalchemy import create_engine
 from main import *
 
 engine = create_engine('postgresql://albamolina@localhost/vidrio')
-
-
 """2. (Python) Read the ex_input file and save its content to database."""
-
 df_ex_input = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx')
-# df_ex_input.to_sql('Ex_input_file_02.04.2021.xlsx', con=engine)
-
+df_ex_input.to_sql('Ex_input_file_02.04.2021.xlsx', con=engine)
 """3. (Python) Read the mapping file and save its content to database."""
-
 df_ex_mapping = pd.read_excel('data/Ex_mapping_file.xlsx')
-# df_ex_mapping.to_sql('data/Ex_mapping_file.xlsx', con=engine)
-
-
-"""NOTE about step 2 and step 3 
-1. This "gets the job done through "brute force"  but I want to refactor this tomorrow morning to have it be a for loop with a list of nested dictionaries instead for the files, this way the forloop can unpack those dictionaries
-2. I want to also make the pd.read_excel() method table naming dynamic - 
-- I also think this can come together with the for loop/unpacking the list of dicts
-3. Furthermore, to build on step 2- I will make it dynamic based on the renaming of the file to fit the format that step 1 asks 
-"""
+df_ex_mapping.to_sql('data/Ex_mapping_file.xlsx', con=engine)
 
 # 4
 
@@ -31,8 +18,3 @@ df_ex_mapping = pd.read_excel('data/Ex_mapping_file.xlsx')
 headers = df_ex_input.columns 
 
 reference_day = df_ex_input['LAST_UPDATE_DATE_EOD']
-print(reference_day)
-
-"""
-Reference Day: taken from the time stamp portion of the input data file 
-bc last time they put it in """
---
+++ num4.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from main import *
+
+engine = create_engine('postgresql://albamolina@localhost/vidrio')
+"""2. (Python) Read the ex_input file and save its content to database."""
+df_ex_input = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx')
+# df_ex_input.to_sql('Ex_input_file_02.04.2021.xlsx', con=engine)
+"""3. (Python) Read the mapping file and save its content to database."""
+df_ex_mapping = pd.read_excel('data/Ex_mapping_file.xlsx')
+# df_ex_mapping.to_sql('data/Ex_mapping_file.xlsx', con=engine)
+
+
+
+"""
+(Python) Using “pandas” package to generate portfolio valuation output file
+(“DAV Proforma Acc Analy.xlsx”) that will have the following columns (col names in bold, specs below):
+
+Reference Day: taken from the time stamp portion of the input data file
+Periodicity: ‘Daily’
+
+Investor Account UID: “HFRIILAU” from column B in the “Index Data” tab of the input data file
+
+Investor Account Long Name: From column C in the “Index Data” tab of the input data file where ISIN =
+“HFRIILAU” and Date= Reference Day
+Investment Account UID: Leave blank
+Investment Account Long Name: Have to translate company name from “Constituents” tab of the data
+input file to Product Name from the mapping file using “ISIN” column from the “Constituents” tab and
+“Counterparty ID” column in the mapping file as matching keys. Use these keys to match all records in
+the output data set.
+Attribution Gross: “Gross Contribution to Index” column from “Constituents” tab
+Attribution Net: “Net Contribution to Index” column from “Constituents” tab
+Opening Allocation: 'Beginning Weight %' column from “Constituents” tab
+Closing Allocation: 'End Weight %' column from “Constituents” tab
+Opening Equity: ‘Previous Day NAV’ column from Index Data tab for ‘HFRI-I Liquid Alt UCITS Index’ index
+for Reference Day
+Closing Equity: ‘NAV’ column from Index Data tab for ‘HFRI-I Liquid Alt UCITS Index’ index for Reference
+Day
+Investment Performance: '% Price Change' column from “Constituents” tab
+Investment Adj Opening Balance: 'Opening Allocation' * 'Opening Equity'
+Investment Closing Balance: 'Closing Allocation' * 'Closing Equity'
+Portfolio Opening Balance: 'Investment Adj Opening Balance'"""
+
+
+# 4
+
+# print(df_ex_input.columns, "=====> df ex col",
+# df_ex_mapping.columns,  "=====> df ex mapping col",)
+
+headers = df_ex_input.columns 
+print(headers)
+
+reference_day = df_ex_input['LAST_UPDATE_DATE_EOD']
+print(reference_day)
+
+for index, row in df_ex_input.iterrows():
+    print(index,row['Current NAV'])
