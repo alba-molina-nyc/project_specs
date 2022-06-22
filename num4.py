8736305b66dbb8c61910c829f811fe188694f96c
Username <albavmolinanyc@gmail.com>
2022-06-22 11:01:07 -0400

    get constituents, index data sheets and the ex_input file in general

--- num4.py
+++ num4.py
@@ -4,12 +4,15 @@ from main import *
 import re
 
 engine = create_engine('postgresql://albamolina@localhost/vidrio')
-"""2. (Python) Read the ex_input file and save its content to database."""
-df_ex_input = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx', 'Constituents')
-# print(df_ex_input)
+# gets the file in general
+df_input_file = df_ex_input = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx')
+print(df_input_file, 'input file general')
 
-df_ex_input2 = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx', 'Index Data')
-print(df_ex_input2)
+df_constituents = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx', 'Constituents')
+print(df_constituents, 'df constituents')
+
+df_index_data = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx', 'Index Data')
+print(df_index_data, 'df index data')
 # df_ex_input.to_sql('Ex_input_file_02.04.2021.xlsx', con=engine)
 """3. (Python) Read the mapping file and save its content to database."""
 df_ex_mapping = pd.read_excel('data/Ex_mapping_file.xlsx')
