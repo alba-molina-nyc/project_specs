c949c32753122724d3ec86bddd9e17a99f828fb7
Username <albavmolinanyc@gmail.com>
2022-06-23 11:56:38 -0400

    cleans up modules a bit - shifts getcols funcs over to merge files and starts generate portfolio function

--- generate_portfolio.py
+++ generate_portfolio.py
@@ -1,25 +1,13 @@
 import pandas as pd
 from read_excel import *
 from importsql import *
+from merge_files import *
+from change_filename import * 
 
-def get_c_cols():
-    df_constituents_data = read_c()
-    df_constituents_data = df_constituents_data.drop(columns=[ 'FUND_COMP_NAME', 'LONG_COMP_NAME', 'Strategy', 'Substrategy', 'Currency Denomination', 'Red Notice ', 'Red Settlement ', 'Sub Notice ', 'Sub Settlement ', 'Total Fund AUM','Beginning Weight $', 'End Weight $', 'Trade_$_EoD', 'Attribution Gross $','Attribution Net $', 'Attribution Gross %', 'Attribution Net %', 'Current Price', 'Current NAV', 'Previous Price', 'Previous NAV', 'PX_CLOSE_DT', 'FUND_TOTAL_ASSETS','FUND_TOTAL_ASSETS_DT', 'NAV % Change', 'BBG Total Return'])
-    df_constituents_data.columns
-    return df_constituents_data
-get_c_cols()
+def generate_portfolio():
+    portfolio = merge_files()
+    print(portfolio)
+    print(portfolio.columns)
 
-def get_i_cols():
-    df_index_data = read_id()  
-    df_index_data = df_index_data.drop(columns=['Total AUM', 'NAV Change $', 'AUM Change from Previous Day','Previous Day AUM' ])
-    df_index_data.columns
-    return df_index_data
 
-get_i_cols()
-
-def get_m_cols():
-    df_ex_mapping = read_mapping()
-    cols = list(df_ex_mapping.columns)
-    return cols
-
-get_m_cols()
+generate_portfolio()
--- merge_files.py
+++ merge_files.py
@@ -2,18 +2,35 @@ from operator import index
 import pandas as pd
 from read_excel import *
 from importsql import *
-from generate_portfolio import *
 from change_filename import *
 
+def get_c_cols():
+    df_constituents_data = read_c()
+    df_constituents_data = df_constituents_data.drop(columns=[ 'FUND_COMP_NAME', 'LONG_COMP_NAME', 'Strategy', 'Substrategy', 'Currency Denomination', 'Red Notice ', 'Red Settlement ', 'Sub Notice ', 'Sub Settlement ', 'Total Fund AUM','Beginning Weight $', 'End Weight $', 'Trade_$_EoD', 'Attribution Gross $','Attribution Net $', 'Attribution Gross %', 'Attribution Net %', 'Current Price', 'Current NAV', 'Previous Price', 'Previous NAV', 'PX_CLOSE_DT', 'FUND_TOTAL_ASSETS','FUND_TOTAL_ASSETS_DT', 'NAV % Change', 'BBG Total Return'])
+    df_constituents_data.columns
+    return df_constituents_data
+get_c_cols()
 
-# merge files on 'NAV Change % 
+def get_i_cols():
+    df_index_data = read_id()  
+    df_index_data = df_index_data.drop(columns=['Total AUM', 'NAV Change $', 'AUM Change from Previous Day','Previous Day AUM' ])
+    df_index_data.columns
+    return df_index_data
 
+get_i_cols()
 
+def get_m_cols():
+    df_ex_mapping = read_mapping()
+    cols = list(df_ex_mapping.columns)
+    return cols
+
+get_m_cols()
+
+# merge const + index tab on 'NAV Change % 
 def merge_files():
     index_file = get_c_cols()
     constituents_file = get_i_cols()
-    merged_file = index_file.merge(constituents_file[['Date', 'ISIN ', 'Index Name', 'NAV', 'Previous Day NAV',
-       'NAV Change %']], on='NAV Change %', how = 'left')
+    merged_file = index_file.merge(constituents_file[['Date', 'ISIN ', 'Index Name', 'NAV', 'Previous Day NAV', 'NAV Change %']], on='NAV Change %', how = 'left')
     return merged_file
 
 merge_files()
