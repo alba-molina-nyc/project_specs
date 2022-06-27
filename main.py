b1c3c44a23f8fa39c441b752ebce22acb9c77c8b
Username <albavmolinanyc@gmail.com>
2022-06-27 16:29:05 -0400

    last minute updates for review

--- main.py
+++ main.py
@@ -15,13 +15,10 @@ buf= '/'.join(match.group(2)[:-1].split('.'))
 
 #2 read data from input and mapping file and 
 ifile_data = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx', sheet_name=['Constituents', 'Index Data'], parse_dates=True)
-
-
 constitutents = (ifile_data['Constituents'])
-# print((constitutents.columns))
 index = ifile_data['Index Data']
-print(index.columns)
-print(index['Previous Day NAV'])
+# print(index.columns)
+# print(index['Previous Day NAV'])
 
 
 ifile_data = constitutents.merge(index, how='outer', right_on='Index Name', left_on='ISIN ')
@@ -39,38 +36,36 @@ for col in temp_df.columns:
     temp_df['Reference Day'] = temp_df['LAST_UPDATE_DATE_EOD']
     temp_df['Periodicity'] = 'Daily'
     temp_df['Investor Account UID'] = temp_df['ISIN '] == 'HFRIILAU' 
-    # temp_df['Investor Account Long Name'] = temp_df['ISIN '] == 'HFRIILAU' & temp_df['Date'] == temp_df['Reference Day']      # to do figure out how to useregex to change the date format
-    # temp_df['Investor Account Long Name'] = ''
+    #TODO figure out how to useregex to change the date format
+    # temp_df['Investor Account Long Name'] = (temp_df['ISIN '] == 'HFRIILAU') & (temp_df['Date'] == (temp_df['Reference Day']))   
     temp_df['Investment Account UID'] = ' '
-    # temp_df['Investment Account Long Name'] = temp_df['']           #TODO: come back
+    # temp_df['Investment Account Long Name'] = temp_df['']                     #TODO: come back         
     temp_df['Attribution Gross'] = temp_df['Gross Contribution to Index']
     temp_df['Attribution Net'] = temp_df['Net Contribution to Index']
     temp_df['Opening Allocation'] = temp_df['End Weight %']
     temp_df['Closing Allocation'] = temp_df['End Weight %']
-    # temp_df['Opening Equity'] = temp_df['Index Name'] == 'HFRI-I Liquid Alt UCITS Index\n(Net)' & temp_df['Date'] == temp_df['Reference Day']             # to do figure out how to useregex to change the date format OR figure out how to use datetime
-    # temp_df['Opening Equity'] = temp_df.iloc[row, col]
-    print(temp_df.columns)
-    counter = 0
-    for col in temp_df.columns:
-        print(col)
-        counter += 1
-        print(counter, col)
-        continue
-
-
-
-#    Opening Equity: ‘Previous Day NAV’ column from Index Data tab for ‘HFRI-I Liquid Alt UCITS Index’ index
-#     for Reference Day
-    # temp_df['Opening Equity'] = temp_df['ISIN '] == 'HFRIILAU' & temp_df['Previous Day Nav'] 
-
-                      #TODO: come back 'Previous Day NAV' & temp_df['ISIN '] == 'HFRIILAU'
-    # temp_df['Closing Equity'] = temp_df['']                         #TODO: come back
+    #TODO figure out how to useregex to change the date format OR figure out how to use datetime
+    # temp_df['Opening Equity'] = (temp_df['Index Name'] == 'HFRI-I Liquid Alt UCITS Index\n(Net)') & (temp_df['Date'] == temp_df['Reference Day'])            
+    #TODO: figure out how to useregex to change the date format OR figure out how to use datetime                   
+    # temp_df['Closing Equity'] = (temp_df['NAV'] == temp_df['Index Name'] == 'HFRI-I Liquid Alt UCITS Index\n(Net)') & (temp_df['Date'] == temp_df['Reference Day'])           
     temp_df['Investment Performance'] = temp_df['% Price Change']
     # temp_df['Investment Adj Opening Balance'] = temp_df['']         #TODO: come back
     # temp_df['Investment Closing Balance'] = temp_df['']             #TODO: come back
     # temp_df['Portfolio Opening Balance'] = temp_df['']              #TODO: come back
     # temp_df['Portfolio Closing Balance'] = temp_df['']              #TODO: come back
-    # print(temp_df.columns)
-  
-    
     break
+
+
+# print(temp_df.columns)
+#     counter = 0
+#     for col in temp_df.columns:
+#         print(col)
+#         counter += 1
+#         print(counter, col)
+#         continue
+
+# start pi
+# y = temp_df.loc[temp_df['Date'].str.contains('^-[a-z]*', regex=True)]
+# print(y,'y')
+# print(temp_df['Date'] )
+# ('^pi[a-z]')
