098e7b69dc1c81e910f26e12d38a0258ed6a262a
Username <albavmolinanyc@gmail.com>
2022-06-27 12:25:45 -0400

    concatenate const and index for smoother merge

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from sqlalchemy import create_engine
 import pandas as pd
 sys.path
+
 engine = create_engine('postgresql://albamolina@localhost/vidrio')
 
 #1 extract the time stamp portion from the input file & display it in MM/DD/YYYY format
@@ -11,13 +12,17 @@ match = re.match('(^[a-zA-Z_/]+)([0-9.]+)(.+$)',str)
 buf= '/'.join(match.group(2)[:-1].split('.'))
 
 #2 read data from input and mapping file and 
-ifile_data = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx')
+ifile_data = pd.read_excel('data/Ex_input_file_02.04.2021.xlsx', sheet_name=['Constituents', 'Index Data'])
+constitutents = ifile_data['Constituents']
+index = ifile_data['Index Data']
+ifile = pd.concat([constitutents, index])
+
 mapping_data = pd.read_excel('data/Ex_mapping_file.xlsx')
 
 #3 save data from input and mapping file and save it to database
-ifile_data.to_sql('data/Ex_input_file_02.04.2021.xlsx', con=engine)
-mapping_data.to_sql('data/Ex_mapping_file.xlsx', con=engine)
+# ifile_data.to_sql('data/Ex_input_file_02.04.2021.xlsx', con=engine)
+# mapping_data.to_sql('data/Ex_mapping_file.xlsx', con=engine)
 
 #4 generate portfolio valuation output file
-temp_df = ifile_data.merge(mapping_data, how='left', right_on='Counterparty ID', left_on='ISIN ')
-print(temp_df)
+temp_df = ifile.merge(mapping_data, how='left', right_on='Counterparty ID', left_on='ISIN ')
+print(len(temp_df.columns))
